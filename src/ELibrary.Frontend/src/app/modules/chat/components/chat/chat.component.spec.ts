3bca4cb520a4fdf5fdc822878143c45abcfc2805
Eugene <pshonovskij@gmail.com>
2024-10-28 21:17:19 +0200

    Updated chat component and tests

--- src/ELibrary.Frontend/src/app/modules/chat/components/chat/chat.component.spec.ts
+++ src/ELibrary.Frontend/src/app/modules/chat/components/chat/chat.component.spec.ts
@@ -178,7 +178,7 @@ describe('ChatComponent', () => {
     const message = { text: "BookId:123 'The Hobbit'", isSent: false };
     const formattedMessage = component.formatChatMessage(message) as string;
 
-    expect(formattedMessage.toString()).toContain('<div><a class="book-link" href="123">The Hobbit</a></div>');
+    expect(formattedMessage.toString()).toContain('<span><a class="book-link" href="123">The Hobbit</a></span>');
   });
 
   it('should detect if a message is a book link', () => {
